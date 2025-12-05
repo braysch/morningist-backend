5a7091d9b6508d3d7f5d4524a52fe92061369950
brayschway <bradanschwanke@gmail.com>
2025-12-04 18:17:33 -0700

    Was previously using hard-coded value for local MongoDB. Replaced with MongoDB Atlas env variable.

--- app.js
+++ app.js
@@ -17,38 +17,39 @@ const contactRouter = require("./routes/contact");
 const { verifyToken, isAdmin } = require("./middleware/auth-middleware");
 
 app.use(cors());
-app.use('/api/stripe/webhook', webhookRouter);
+app.use("/api/stripe/webhook", webhookRouter);
 app.use(express.json());
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: "GET,POST",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: "GET,POST",
+    credentials: true,
+  })
+);
 
-app.get("/",(req, res) => {
-    res.send("Server running");
+app.get("/", (req, res) => {
+  res.send("Server running");
 });
 
 app.use("/category", categoryRouter); // , verifyToken, isAdmin,
-app.use("/subcategory", subcategoryRouter);  // , verifyToken, isAdmin,
-app.use("/product", productRouter);  // , verifyToken, isAdmin,
-app.use("/customer", customerRouter);  // , verifyToken,
+app.use("/subcategory", subcategoryRouter); // , verifyToken, isAdmin,
+app.use("/product", productRouter); // , verifyToken, isAdmin,
+app.use("/customer", customerRouter); // , verifyToken,
 app.use("/auth", authRouter);
-app.use('/api/stripe', stripeRouter);
-app.use('/contact', contactRouter);
+app.use("/api/stripe", stripeRouter);
+app.use("/contact", contactRouter);
 
 async function connectDB() {
-    mongoose.connect("mongodb://localhost:27017/morningist", {
-        dbName: "morningist-store-db",
-    });
-    console.log("Connected to MongoDB", mongoose.connection.host);
+  mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "morningist-store-db",
+  });
+  console.log("Connected to MongoDB", mongoose.connection.host);
 }
 connectDB().catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+  console.error("Error connecting to MongoDB:", err);
 });
 
-
-app.listen(port,()=>{
-    console.log("Server running on port", port);
+app.listen(port, () => {
+  console.log("Server running on port", port);
 });
